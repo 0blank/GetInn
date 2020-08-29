758e8a30e7d4004c9246bd27d9bb8106a31f5c42
0blank <sumangauchan5@gmail.com>
2020-08-29 11:26:54 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { overrideThemeVariables, Button } from "ui-neumorphism";
+import "ui-neumorphism/dist/index.css";
+import "./assets/scss/base.scss";
 
 function App() {
+  useEffect(() => {
+    overrideThemeVariables({
+      "--light-bg": "#E9B7B9",
+      "--light-bg-dark-shadow": "#ba9294",
+      "--light-bg-light-shadow": "#ffdcde",
+      "--dark-bg": "#292E35",
+      "--dark-bg-dark-shadow": "#21252a",
+      "--dark-bg-light-shadow": "#313740",
+      "--primary": "#ac7c44",
+      "--primary-dark": "#4526f9",
+      "--primary-light": "#c7befd",
+    });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>This is NepHotel App</p>
+      <div style={{ margin: 20 }}>
+        <Button>default</Button>
+      </div>
+      <div style={{ margin: 20 }}>
+        <Button color="#ccc" bgColor="var(--primary)">
+          colored
+        </Button>
+      </div>
+      <div style={{ margin: 20 }}>
+        <Button rounded>rounded</Button>
+      </div>
     </div>
   );
 }
