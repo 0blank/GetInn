62527ba807ea8d31f3ab80d4570e7e5535a7e391
0blank <sumangauchan5@gmail.com>
2020-08-29 11:34:19 +0545

    Updates

---
+++ precache-manifest.a1e63ff006e98baa7cd17a0bb2457517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdff6c98b894d1a70005bf991d607a5",
+    "url": "/GetInn/index.html"
+  },
+  {
+    "revision": "70ccc4ff1a6065bc55d6",
+    "url": "/GetInn/static/css/2.2774b781.chunk.css"
+  },
+  {
+    "revision": "5844ccbebbb93bf58f16",
+    "url": "/GetInn/static/css/main.2e1c17e2.chunk.css"
+  },
+  {
+    "revision": "70ccc4ff1a6065bc55d6",
+    "url": "/GetInn/static/js/2.73d3bc11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GetInn/static/js/2.73d3bc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5844ccbebbb93bf58f16",
+    "url": "/GetInn/static/js/main.ebfdddd4.chunk.js"
+  },
+  {
+    "revision": "58c85b8455478f20b7f0",
+    "url": "/GetInn/static/js/runtime-main.1303dfc1.js"
+  }
+]);
